3af857588029e99fe5e1dde8bc285646253d5703
DanielRasho <ray22933@uvg.edu.gt>
2024-10-04 11:41:25 -0600

    feat: create grid entity and partially construct dioarmaa

--- src/internal/entitiy/grid.rs
+++ src/internal/entitiy/grid.rs
@@ -1,35 +1,61 @@
-/*
-
 use nalgebra_glm::Vec3;
-use super::color::Color;
+use std::sync::Arc;
 use super::material::Material;
 use super::cube::Cube;
-
-// Assuming you already have the Cube struct and the Object trait from the previous code
+use super::object::Object; // Assuming you have an Object trait defined
 
 pub struct Grid {
-    pub cubes: Vec<Cube>, // A list of cubes in the grid
+    pub cube_width: f32,
+    pub height: usize,
+    pub width: usize,
+    pub depth: usize,
+    pub objects: Vec<Box<dyn Object + Sync>>, // Change to store objects instead of cubes
 }
 
 impl Grid {
-    pub fn new(width: f32, corner_points: Vec<Vec3>, material: Material) -> Grid {
-        // Create an empty list to store the cubes
-        let mut cubes = Vec::new();
-
-        // Iterate over the provided corner points
-        for point in corner_points {
-            // Calculate the opposite corner of the cube
-            let max_corner = point + Vec3::new(width, width, width);
-
-            // Create a new cube and add it to the list
-            let cube = Cube::new(point, max_corner, material.clone());
-            cubes.push(cube);
+    pub fn new(cube_width: f32, grid_height: usize, grid_width: usize, grid_depth: usize) -> Grid {
+        Grid {
+            cube_width,
+            height: grid_height,
+            width: grid_width,
+            depth: grid_depth,
+            objects: Vec::new(), // Initialize objects vector
         }
+    }
 
-        // Return the grid containing all the cubes
-        Grid { cubes }
+    pub fn add_cube(&mut self, x: usize, y: usize, z: usize, material: Arc<Material>) {
+        // Check if coordinates are within the grid bounds
+        let grid_x_half = self.width as f32 / 2.0;
+        let grid_y_half = self.height as f32 / 2.0;
+        let grid_z_half = self.depth as f32 / 2.0;
+
+        if x < self.width && y < self.height && z < self.depth {
+            let min = Vec3::new(
+                (x as f32) - grid_x_half * self.cube_width,
+                (y as f32) - grid_y_half * self.cube_width,
+                (z as f32) - grid_z_half * self.cube_width,
+            );
+            let max = Vec3::new(
+                min.x + self.cube_width,
+                min.y + self.cube_width,
+                min.z + self.cube_width,
+            );
+
+            let cube = Cube::new(min, max, material);
+            self.objects.push(Box::new(cube)); // Store the cube as a Box<dyn Object + Sync>
+        } else {
+            eprintln!("Attempted to add a cube out of grid bounds.");
+        }
+    }
+
+    pub fn add_layer(&mut self, layer: usize, list: &[(usize, usize, Arc<Material>)]) {
+        // Check if the layer is within bounds
+        if layer < self.height {
+            for &(x, z, ref material) in list {
+                self.add_cube(x, layer, z, Arc::clone(material));
+            }
+        } else {
+            eprintln!("Attempted to add cubes to a layer out of grid bounds.");
+        }
     }
 }
-
-
-*/
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ mod internal;
 use internal::camera::Camera;
 use minifb::{Window, WindowOptions, Key};
 use nalgebra_glm::Vec3;
-use std::alloc::GlobalAlloc;
 use std::sync::Arc;
 use std::time::Duration;
 use std::f32::consts::PI;
@@ -14,7 +13,8 @@ use internal::entitiy::material::{Material, Diffuse};
 use internal::entitiy::cube::Cube;
 use internal::entitiy::object::Object;
 use internal::entitiy::light::{AmbientLight, DayLight, Light, PointLight};
-use internal::entitiy::texture::{BOOK_SHELF, CHEST, CRAFTING_TABLE, FURNACE, GLASS, GRASS, OAK_LOG, OAK_PLANKS, JUKEBOX};
+use internal::entitiy::texture::{BOOK_SHELF, CHEST, COBBLESTONE, CRAFTING_TABLE, FURNACE, GLASS, GRASS, JUKEBOX, OAK_LOG, OAK_PLANKS, DARK_OAK_PLANKS};
+use internal::entitiy::grid::{self, Grid};
 
 pub fn start(){
     
@@ -37,31 +37,247 @@ pub fn start(){
     ).unwrap();
     
     // Create an array of Box<dyn Object>
-    let white_fur = Arc::new( Material {
-        diffuse: Diffuse::Texture(FURNACE.clone()),
+    let grass = Arc::new( Material {
+        diffuse: Diffuse::Texture(GRASS.clone()),
         specular: 10.0,
         albedo: [0.6, 0.3],
-        reflectivity: 0.2,
+        reflectivity: 0.0,
         transparency: 0.0,
         refractive_index: 1.0,
     });
     
-    let black_fur = Arc::new (Material {
-        diffuse: Diffuse::Texture(JUKEBOX.clone()),
+    let cobbleston = Arc::new (Material {
+        diffuse: Diffuse::Texture(COBBLESTONE.clone()),
         specular: 10.0,
         albedo: [0.9, 0.1],
         reflectivity: 0.2,
-        transparency: 1.0,
+        transparency: 0.0,
         refractive_index: 1.0,
     });
 
-    let objects: [Box<dyn Object + Sync>; 2] = [
-        Box::new(Cube{ max: Vec3::new(0.5,0.5,0.5), min: Vec3::new(-0.5, -0.5, -0.5), material: Arc::clone(&white_fur)}), // EARS
-        Box::new(Cube{ max: Vec3::new(1.0,2.0,2.5), min: Vec3::new(0.0, 1.0, 1.5), material: Arc::clone(&black_fur)}), // EARS
-    ];
+    let oak_log = Arc::new (Material {
+        diffuse: Diffuse::Texture(OAK_LOG.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let oak_planks = Arc::new (Material {
+        diffuse: Diffuse::Texture(OAK_PLANKS.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let dark_oak_planks = Arc::new (Material {
+        diffuse: Diffuse::Texture(OAK_PLANKS.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let furnace = Arc::new (Material {
+        diffuse: Diffuse::Texture(FURNACE.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let chest = Arc::new (Material {
+        diffuse: Diffuse::Texture(CHEST.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let crafting_table = Arc::new (Material {
+        diffuse: Diffuse::Texture(CRAFTING_TABLE.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let glass = Arc::new (Material {
+        diffuse: Diffuse::Texture(GLASS.clone()),
+        specular: 10.0,
+        albedo: [0.9, 0.1],
+        reflectivity: 0.2,
+        transparency: 0.0,
+        refractive_index: 1.0,
+    });
+
+    let mut diorama = Grid::new(1.0, 9, 11, 10);
+    diorama.add_layer(0, &vec![
+        (0, 0, Arc::clone(&grass)),
+        (1, 0, Arc::clone(&grass)),
+        (2, 0, Arc::clone(&grass)),
+        (3, 0, Arc::clone(&grass)),
+        (4, 0, Arc::clone(&grass)),
+        (5, 0, Arc::clone(&grass)),
+        (6, 0, Arc::clone(&grass)),
+        (7, 0, Arc::clone(&grass)),
+        (8, 0, Arc::clone(&grass)),
+
+        (0, 1, Arc::clone(&grass)),
+        (0, 2, Arc::clone(&grass)),
+        (0, 3, Arc::clone(&grass)),
+        (0, 4, Arc::clone(&grass)),
+        (0, 5, Arc::clone(&grass)),
+        (0, 6, Arc::clone(&grass)),
+        (0, 7, Arc::clone(&grass)),
+
+        (8, 1, Arc::clone(&grass)),
+        (8, 2, Arc::clone(&grass)),
+        (8, 3, Arc::clone(&grass)),
+        (8, 4, Arc::clone(&grass)),
+        (8, 5, Arc::clone(&grass)),
+        (8, 6, Arc::clone(&grass)),
+        (8, 7, Arc::clone(&grass)),
+        (8, 8, Arc::clone(&grass)),
+        (8, 9, Arc::clone(&grass)),
+
+        (2, 9, Arc::clone(&grass)),
+        (3, 9, Arc::clone(&grass)),
+        (4, 9, Arc::clone(&grass)),
+        (5, 9, Arc::clone(&grass)),
+        (6, 9, Arc::clone(&grass)),
+        (7, 9, Arc::clone(&grass)),
+       
+        (1, 8, Arc::clone(&grass)),
+        (1, 7, Arc::clone(&grass)),
+        (2, 8, Arc::clone(&grass)),
+    ]);
+
+    diorama.add_layer(1, &vec![
+        (1, 1, Arc::clone(&oak_log)),
+        (7, 1, Arc::clone(&oak_log)),
+        (7, 8, Arc::clone(&oak_log)),
+        
+        
+        (1, 2, Arc::clone(&cobbleston)),
+        (1, 3, Arc::clone(&cobbleston)),
+        (1, 4, Arc::clone(&cobbleston)),
+        (1, 5, Arc::clone(&cobbleston)),
+        (1, 6, Arc::clone(&cobbleston)),
+
+        (2, 1, Arc::clone(&cobbleston)),
+        (3, 1, Arc::clone(&cobbleston)),
+        (4, 1, Arc::clone(&cobbleston)),
+        (5, 1, Arc::clone(&cobbleston)),
+        (6, 1, Arc::clone(&cobbleston)),
+
+        (7, 2, Arc::clone(&cobbleston)),
+        (7, 3, Arc::clone(&cobbleston)),
+        (7, 4, Arc::clone(&cobbleston)),
+        (7, 5, Arc::clone(&cobbleston)),
+        (7, 6, Arc::clone(&cobbleston)),
+        (7, 7, Arc::clone(&cobbleston)),
+
+        (3, 8, Arc::clone(&cobbleston)),
+        (4, 8, Arc::clone(&cobbleston)),
+        (5, 8, Arc::clone(&cobbleston)),
+        (6, 8, Arc::clone(&cobbleston)),
+
+        (2, 7, Arc::clone(&cobbleston)),
+        (3, 7, Arc::clone(&cobbleston)),
+        (4, 7, Arc::clone(&cobbleston)),
+        (5, 7, Arc::clone(&cobbleston)),
+
+        (2, 6, Arc::clone(&cobbleston)),
+        (3, 6, Arc::clone(&cobbleston)),
+        (4, 6, Arc::clone(&cobbleston)),
+        (5, 6, Arc::clone(&cobbleston)),
+
+        (2, 5, Arc::clone(&cobbleston)),
+        (3, 5, Arc::clone(&cobbleston)),
+        (4, 5, Arc::clone(&cobbleston)),
+        (5, 5, Arc::clone(&cobbleston)),
+
+        (3, 4, Arc::clone(&cobbleston)),
+        (4, 4, Arc::clone(&cobbleston)),
+        (5, 4, Arc::clone(&cobbleston)),
+        (6, 4, Arc::clone(&cobbleston)),
+
+        (3, 3, Arc::clone(&cobbleston)),
+        (4, 3, Arc::clone(&cobbleston)),
+        (5, 3, Arc::clone(&cobbleston)),
+        (6, 3, Arc::clone(&cobbleston)),
+
+        (2, 2, Arc::clone(&cobbleston)),
+        (3, 2, Arc::clone(&cobbleston)),
+        (4, 2, Arc::clone(&cobbleston)),
+        (5, 2, Arc::clone(&cobbleston)),
+        (6, 2, Arc::clone(&cobbleston)),
+    ]);
+
+    diorama.add_layer(2, &vec![
+        (1, 1, Arc::clone(&oak_log)),
+        (7, 1, Arc::clone(&oak_log)),
+        (7, 8, Arc::clone(&oak_log)),
+
+        (1, 2, Arc::clone(&oak_planks)),
+        (1, 3, Arc::clone(&oak_planks)),
+        (1, 4, Arc::clone(&oak_planks)),
+        (1, 5, Arc::clone(&oak_planks)),
+
+        (2, 1, Arc::clone(&oak_planks)),
+        (3, 1, Arc::clone(&oak_planks)),
+        (5, 1, Arc::clone(&oak_planks)),
+        (6, 1, Arc::clone(&oak_planks)),
+
+        (7, 2, Arc::clone(&oak_planks)),
+        (7, 3, Arc::clone(&oak_planks)),
+        (7, 4, Arc::clone(&oak_planks)),
+        (7, 5, Arc::clone(&oak_planks)),
+        (7, 6, Arc::clone(&oak_planks)),
+        (7, 7, Arc::clone(&oak_planks)),
+        
+        (2, 3, Arc::clone(&crafting_table)),
+        (2, 4, Arc::clone(&chest)),
+
+        (6, 5, Arc::clone(&furnace)),
+        (6, 6, Arc::clone(&furnace)),
+
+        (6, 7, Arc::clone(&dark_oak_planks)),
+    ]);
+
+    diorama.add_layer(3, &vec![
+        (1, 1, Arc::clone(&oak_log)),
+        (7, 1, Arc::clone(&oak_log)),
+        (7, 8, Arc::clone(&oak_log)),
+
+        (1, 2, Arc::clone(&oak_planks)),
+        (1, 3, Arc::clone(&oak_planks)),
+
+        (2, 1, Arc::clone(&glass)),
+        (3, 1, Arc::clone(&oak_planks)),
+        (5, 1, Arc::clone(&oak_planks)),
+        (6, 1, Arc::clone(&glass)),
+
+        (7, 2, Arc::clone(&oak_planks)),
+        (7, 3, Arc::clone(&oak_planks)),
+        (7, 4, Arc::clone(&oak_planks)),
+        (7, 5, Arc::clone(&oak_planks)),
+        (7, 6, Arc::clone(&oak_planks)),
+        (7, 7, Arc::clone(&oak_planks)),
+    ]);
+    
+    diorama.add_layer(4, &vec![]);
     
     let mut camera = Camera::new(
-        Vec3::new(0.0, 0.0, 15.0),
+        Vec3::new(0.0, 0.0, 20.0),
         Vec3::new(0.0, 0.0, 0.0),
         Vec3::new(0.0, 1.0, 0.0)
     );
@@ -88,7 +304,7 @@ pub fn start(){
         Vec3::new(0.0,0.0,0.0),
         10.0, 0.0, 
         Color::new(255, 255, 255), 
-        3.0
+        1.0
     );
     
     
@@ -130,16 +346,16 @@ pub fn start(){
         if window.is_key_down(Key::M) {
             sun.translate_day_light(DAY_SPEED);
             day_angle = (day_angle + DAY_SPEED) % (2.0 * PI);
-            render(&mut framebuffer, &objects, &camera, &lights, &sun, &ambient_light);
+            render(&mut framebuffer, &diorama.objects, &camera, &lights, &sun, &ambient_light);
         }
         if window.is_key_down(Key::N) {
             sun.translate_day_light(-DAY_SPEED);
             day_angle = (day_angle - DAY_SPEED) % (2.0 * PI);
-            render(&mut framebuffer, &objects, &camera, &lights, &sun, &ambient_light);
+            render(&mut framebuffer, &diorama.objects, &camera, &lights, &sun, &ambient_light);
         }
 
         if camera.check_if_changed() {
-            render(&mut framebuffer, &objects, &camera, &lights, &sun, &ambient_light);
+            render(&mut framebuffer, &diorama.objects, &camera, &lights, &sun, &ambient_light);
         }
 
         window
@@ -150,3 +366,4 @@ pub fn start(){
     }
   
 }
+
